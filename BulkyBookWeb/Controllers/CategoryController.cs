8df70018f2680fbdd6410f2f6eef0fb9c9e63bee
Lashon Hudson <lashonhud@gmail.com>
2023-06-11 16:32:15 -0400

    Add project files.

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using BulkyBookWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var odjCategoryList = _db.Categories.ToList();
+            return View(odjCategoryList);
+        }
+    }
+}
---
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {//Using Dbcontext to utilize entity frame work
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+                
+        }
+        //Telling DbContext to use the table that was created
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
